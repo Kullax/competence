852ae53f44d0f079dd32a5b8e83d1cb910c83fee
Martin <kullax@feral.dk>
2015-10-15 12:05:42 +0200

    Initial Scenes

---
+++ Competence Project/Assets/Scripts/Camera.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+    public Transform target;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(target);
+    }
+}
---
+++ Competence Project/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+
+    public float turnSmoothing = 15f;
+    private CharacterController controller;
+    private Vector3 moveDirection = Vector3.zero;
+    public float speed = 6.0F;
+    public float gravity = 20.0F;
+    public float jumpSpeed = 8.0F;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A))
+            transform.Rotate(new Vector3(0, Input.GetAxis("Horizontal"), 0) * 75f * Time.deltaTime);
+        moveDirection = new Vector3(0, 0, Input.GetAxis("Vertical"));
+        moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection *= speed;
+        if (Input.GetButton("Jump"))
+            moveDirection.y = jumpSpeed;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
+/*
+if (controller.isGrounded)
+{
+    if (!Input.anyKey)
+        return;
+    if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A))
+        transform.Rotate(new Vector3(0, Input.GetAxis("Horizontal"), 0) * 75f * Time.deltaTime);
+    if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+    moveDirection = transform.TransformDirection(moveDirection);
+    moveDirection *= speed;
+
+}
+moveDirection.y -= gravity * Time.deltaTime;
+controller.Move(moveDirection * Time.deltaTime);
+}*/
